c59d969495b73b2ca679390beb54a002a8f7045c
zhane <zhanezaur89@gmail.com>
2022-07-25 21:57:15 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] arg) {
+        System.out.println("Zaur Zhane");
+        System.out.println("05.08.1989");
+    }
+
+}
